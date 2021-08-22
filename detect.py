ec777cbeb4567c98fe2d2ac3df6655e409aacc68
acse-yl13320 <50014026+acse-yl13320@users.noreply.github.com>
2021-08-22 22:49:21 +0100

    update

--- detect.py
+++ detect.py
@@ -82,6 +82,7 @@ def detect(weights='yolov5s.pt',  # model.pt path(s)
     t0 = time.time()
     for path, img, im0s, vid_cap in tqdm(dataset):
         img = torch.from_numpy(img).to(device)
+
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
